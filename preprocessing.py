65feb3fbd899ac6081838621cc93ab65aefeddcf
Jan Pisl <jan.pisl@epfl.ch>
2022-07-06 18:25:40 +0200

    refactor preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -15,14 +15,15 @@ import numpy as np
 #controls = pd.read_csv('data/ILUC_controls_labels.csv')
 campaign = pd.read_csv('data/ILUC_campaign_labels.csv')
 
-folder = 'data/campaign_L8_examples'
 
 
 gdf = gpd.read_file('data/geowiki/ILUC.shp')
 
-campaign = campaign.drop(['Unnamed: 0'], axis=1)
+try:
+    campaign = campaign.drop(['Unnamed: 0'], axis=1)
+except KeyError:
+    pass
 campaign = campaign.set_index('sampleid')
-pdb.set_trace()
 
 # I add geometry to the DataFrame with labels.
 indexed_geoms = gdf[['sampleid', 'geometry']].drop_duplicates().set_index('sampleid')
@@ -30,17 +31,45 @@ filtered_geoms = indexed_geoms.loc[campaign.index]
 campaing_processed = pd.concat([campaign, filtered_geoms], axis=1)
 
 
-paths = []
-for i, row in campaing_processed.iterrows():
-    filename = f'{i}_2020_1.tif'
-    paths.append(filename)
+def has_ambiguous_label(df):
+    """Identify and return boolean filter of rows where two or more classes
+    have identical number of votes (ie. there is no unique class 
+    that has most votes).
 
-campaing_processed['filename'] = paths
+    Args:
+        df (pd.DataFrame): df with 'sampleid' and classes with votes and 'geometry'
 
-drop_indices = []
-for index, row in campaing_processed.iterrows():
-    if not os.path.isfile(os.path.join(folder, row.filename)):
-        drop_indices.append(index)
+    Returns:
+        pd.DataFrame: contains rows with
+    """
+    df = df.set_index('sampleid')
+    df['most_votes'] = df.max(axis=1)
+    df['second_most_votes'] = df.drop(['most_votes', 'geometry'], axis=1).apply(lambda row: row.nlargest(2).values[-1],axis=1)
 
-campaing_processed = campaing_processed.drop(drop_indices)
+    return df.most_votes == df.second_most_votes
+
+
+filtered = campaing_processed[~has_ambiguous_label(campaing_processed)]
+
+
+def drop_if_not_file_exists(dataframe, folder):
+
+    paths = []
+    for i, row in dataframe.iterrows():
+        filename = f'{i}_2020_1.tif'
+        paths.append(filename)
+
+    campaing_processed['filename'] = paths
+
+    drop_indices = []
+    for index, row in dataframe.iterrows():
+        if not os.path.isfile(os.path.join(folder, row.filename)):
+            drop_indices.append(index)
+
+    dataframe = dataframe.drop(drop_indices)
+
+    return dataframe
+
+#folder = 'data/campaign_L8_examples'
+#filtered = drop_if_not_file_exists(campaing_processed, folder)
 
