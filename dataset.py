e5e30e8db0953b643c82e532e2cd009fa0ac2cc7
Jan Pisl <jan.pisl@epfl.ch>
2022-07-07 18:28:31 +0200

    update dataset

--- dataset.py
+++ dataset.py
@@ -1,20 +1,21 @@
 import os
+import pdb
 
 import pandas as pd
 from torch import Tensor
 import rasterio
 
 from torch.utils.data import Dataset
-from preprocessing import drop_if_not_file_exists
+from preprocessing import drop_if_not_file_exists, drop_if_missing_data
 
 class GeoWikiDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, drop_rows_with_missing_file=False, transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, drop_rows_with_missing_file=False, drop_rows_with_nan_data=False, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         
         if drop_rows_with_missing_file:
-            size = len(self.img_labels)
             self.img_labels = drop_if_not_file_exists(self.img_labels, img_dir)
-            print(f'Dropped {size - len(self.img_labels)} rows where the corresponding file was not found. Rows remaining: {len(self.img_labels)}')
+        if drop_rows_with_nan_data:
+            self.img_labels = drop_if_missing_data(self.img_labels, img_dir)
         
         self.img_dir = img_dir
         self.transform = transform
