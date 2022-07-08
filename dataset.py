b32871715239d77e657b27bd80448047ada081f3
Jan Pisl <jan.pisl@epfl.ch>
2022-07-08 18:06:09 +0200

    update dataset

--- dataset.py
+++ dataset.py
@@ -4,13 +4,16 @@ import pdb
 import pandas as pd
 from torch import Tensor
 import rasterio
-
+from torchvision import transforms
+from torch.nn import Sequential
 from torch.utils.data import Dataset
-from preprocessing import drop_if_not_file_exists, drop_if_missing_data
+from preprocessing import drop_if_not_file_exists, drop_if_missing_data, get_file_names
+
 
 class GeoWikiDataset(Dataset):
     def __init__(self, annotations_file, img_dir, drop_rows_with_missing_file=False, drop_rows_with_nan_data=False, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
+        self.img_labels['filename'] = get_file_names(self.img_labels, 'sampleid')
         
         if drop_rows_with_missing_file:
             self.img_labels = drop_if_not_file_exists(self.img_labels, img_dir)
@@ -37,3 +40,18 @@ class GeoWikiDataset(Dataset):
         labels = Tensor([value for _, value in labels.items()])
 
         return image, labels
+
+
+
+def get_image_transform(folder, cropsize=32):
+
+    #skip this to save time in development
+    #means, stds = get_means_stds(folder)
+
+    means = [21.08917549,  26.82532432,  50.31721194,  46.70581121, 224.1870091, 162.03204172,  88.59852001]
+    stds = [6.85905351,  7.88942854, 10.88926628, 15.86582499, 23.3733194, 32.04417448, 26.91564416]
+
+    return Sequential(
+        transforms.CenterCrop(cropsize),
+        transforms.Normalize(means, stds),
+    )
