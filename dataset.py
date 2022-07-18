add tutorial model, add softmax option, fix pytorch model fetch

dataset.py
@@ -1,6 +1,8 @@
 import os
 import pdb
 
+
+import torch
 from imgaug import augmenters as iaa
 import torchvision
 import utm
@@ -67,6 +69,10 @@ class GeoWikiDataset(Dataset):
             self.img_labels = self.img_labels.loc[~has_missing_data(self.img_labels, img_dir)].reset_index(drop=True)
             print(f'Dropped {size - len(self.img_labels)} rows where the corresponding file contained missing data. Rows remaining: {len(self.img_labels)}')  
 
+
+        #Randomly swapping labels as a sanity check
+        #self.img_labels['sampleid'] = self.img_labels[['sampleid']].sample(frac=1).reset_index(drop=True)
+                
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -80,7 +86,7 @@ class GeoWikiDataset(Dataset):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx]['filename'])
         with rasterio.open(img_path) as source:
             image = Tensor(source.read())
-
+        
         labels = self.img_labels.iloc[idx].to_dict()
         if self.transform:
             image = self.transform(image)
@@ -88,17 +94,25 @@ class GeoWikiDataset(Dataset):
             labels = self.target_transform(labels)
 
         labels = Tensor([value for key, value in labels.items() if key in CLASSES])
+        
+        #This is how i tried to feed the model the label as input
+        #image[:,:,:] = torch.argmax(labels).item()
 
-        return image, labels
+        # only using RGB bands 
+        #image = image[1:4,:,:]
+
+        return image, torch.argmax(labels)
 
 
 def get_balanced_classes(df):
     
     labels = df.drop([col for col in df.columns if col not in CLASSES], axis=1)
     classes, counts = np.unique(labels.idxmax(axis=1), return_counts=True)
-    min_count = 10
+
+    #min_count = 5
+    min_count = counts.min()
+
     print(f"Using {min_count} examples per class.")
-    #min_count = counts.min()
     indices = []
     for _class in CLASSES:
         try:
@@ -202,6 +216,7 @@ def get_datasets(annotations_path,
         drop_rows_with_nan_data=drop_missing_vals, #Drop row if any pixel in corresp. image has 0s across all bands
         majority_label_rows_only=True, #This is false for evaluation in order to compute statistics like Prec/Recall/F1-score 
         single_label_rows_only=single_label_only, #Only use rows where all votes are for one class
+        #undersample=undersample, #If True, class balance is forced by only using as many examples from each class that the rarest class has
         transform=test_val_image_transform)
 
     print("\nProcessing test dataset")

model.py
@@ -41,7 +41,7 @@ class ResidualBlock(nn.Module):
 
 
 class ResNet18(nn.Module):
-    def __init__(self, in_channels, classes, output_sigmoid):
+    def __init__(self, in_channels, classes, output_sigmoid=False, output_softmax=False):
         super(ResNet18,self).__init__()
         
         self.block1 = nn.Sequential(
@@ -67,7 +67,9 @@ class ResNet18(nn.Module):
         self.output_sigmoid = output_sigmoid
         if self.output_sigmoid:
             self.sigmoid = nn.Sigmoid()
-        
+
+        self.output_softmax = output_softmax
+
     def forward(self,x):
         x = self.block1(x)
         x = self.block2(x)
@@ -79,11 +81,12 @@ class ResNet18(nn.Module):
 
         if self.output_sigmoid:
             x = self.sigmoid(x)
+        
+        if self.output_softmax:
+            assert self.output_sigmoid is False
+            x = torch.nn.functional.log_softmax(x, dim=1)
 
         return x
-
-
-
 
 
 def get_resnet18_pytorch(in_channels=7, output_classes=9):
@@ -97,65 +100,9 @@ def get_resnet18_pytorch(in_channels=7, output_classes=9):
                           padding=net.conv1.padding,
                           bias=False)
 
-    '''net.layer2[0].conv1 = nn.Conv2d(net.layer2[0].conv1.in_channels, 
-                                    net.layer2[0].conv1.out_channels, 
-                                    kernel_size=net.layer2[0].conv1.kernel_size, 
-                                    stride=(1,1), 
-                                    padding=net.layer2[0].conv1.padding,
-                                    bias=net.layer2[0].conv1.bias)   
-
-    net.layer2[0].downsample[0] = nn.Conv2d(net.layer2[0].downsample[0].in_channels, 
-                                    net.layer2[0].downsample[0].out_channels, 
-                                    kernel_size=net.layer2[0].downsample[0].kernel_size, 
-                                    stride=(1,1), 
-                                    padding=net.layer2[0].downsample[0].padding,
-                                    bias=net.layer2[0].downsample[0].bias)  
-
-
-    #layer3
-    net.layer3[0].conv1 = nn.Conv2d(net.layer3[0].conv1.in_channels, 
-                                    net.layer3[0].conv1.out_channels, 
-                                    kernel_size=net.layer3[0].conv1.kernel_size, 
-                                    stride=(1,1), 
-                                    padding=net.layer3[0].conv1.padding,
-                                    bias=net.layer3[0].conv1.bias)   
-
-    net.layer3[0].downsample[0] = nn.Conv2d(net.layer3[0].downsample[0].in_channels, 
-                                            net.layer3[0].downsample[0].out_channels, 
-                                            kernel_size=net.layer3[0].downsample[0].kernel_size, 
-                                            stride=(1,1), 
-                                            padding=net.layer3[0].downsample[0].padding,
-                                            bias=net.layer3[0].downsample[0].bias)  
-
-    #layer4
-    net.layer4[0].conv1 = nn.Conv2d(net.layer4[0].conv1.in_channels, 
-                                    net.layer4[0].conv1.out_channels, 
-                                    kernel_size=net.layer4[0].conv1.kernel_size, 
-                                    stride=(1,1), 
-                                    padding=net.layer4[0].conv1.padding,
-                                    bias=net.layer4[0].conv1.bias)   
-
-    net.layer4[0].downsample[0] = nn.Conv2d(net.layer4[0].downsample[0].in_channels, 
-                                            net.layer4[0].downsample[0].out_channels, 
-                                            kernel_size=net.layer4[0].downsample[0].kernel_size, 
-                                            stride=(1,1), 
-                                            padding=net.layer4[0].downsample[0].padding,
-                                            bias=net.layer4[0].downsample[0].bias)'''  
-    
-    
-    net.layer2[0].conv1.stride = (1,1)
-    net.layer2[0].downsample[0].stride = (1,1)
-    net.layer3[0].conv1.stride = (1,1)
-    net.layer3[0].downsample[0].stride = (1,1)
-    net.layer4[0].conv1.stride = (1,1)
-    net.layer4[0].downsample[0].stride = (1,1)
-
     net.fc = nn.Linear(512, out_features=output_classes, bias = True)
 
     return net
-
-
-
 
 
 import torch.nn as nn
@@ -181,3 +128,30 @@ class TestNet(nn.Module):
         x = self.fc3(x)
         return x
 
+
+
+class Net(nn.Module):
+    """Adapted from PyTorch MNIST tutorial on GitHub"""
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(7, 64, 3, 1)
+        self.conv2 = nn.Conv2d(64, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(12544, 128)
+        self.fc2 = nn.Linear(128, 9)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        #output = F.log_softmax(x, dim=1)
+        return x